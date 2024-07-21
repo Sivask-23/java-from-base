42cd4d1983766b88e159feaa08a9c4c2520d1c42
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-07-21 21:40:21 +0530

    Add files via upload

---
+++ revision/forLoop.java
@@ -0,0 +1,12 @@
+public class forLoop {
+    public static void main(String[] args) {
+        int counter=0;
+        for (int j=0;counter<10;counter++){
+
+        }
+        System.out.println(counter);
+//        for (;;){ // infinite loop
+//            System.out.println("hy");
+//        }
+    }
+}
