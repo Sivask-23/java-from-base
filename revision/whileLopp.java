ce188d39f5b5859159c384046c7b172685fd54c0
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-07-24 21:47:29 +0530

    Add files via upload

---
+++ revision/whileLopp.java
@@ -0,0 +1,13 @@
+public class whileLopp {
+    public static void main(String[] args) {
+        int counter=0;
+        while(counter!=10){
+        counter++;
+        }
+        System.out.println(counter);
+
+//        while (true){ // infinite loop
+//            System.out.println("Infinite loop");
+//        }
+    }
+}
