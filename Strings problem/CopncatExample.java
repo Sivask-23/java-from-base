dec64d4656d69e29c148739664e9b4d3640c05b6
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-08-01 22:12:32 +0530

    Add files via upload

---
+++ Strings problem/CopncatExample.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class CopncatExample {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println('a'+'b');
+        System.out.println("a"+"b");
+        System.out.println((char) ('a'+3));
+
+    }
+}
