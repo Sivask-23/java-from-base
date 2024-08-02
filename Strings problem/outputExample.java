0b4d0111eaba4c0e8f3c1f65f6a9965601999f50
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-08-02 20:21:07 +0530

    Add files via upload

---
+++ Strings problem/outputExample.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class outputExample {
+    public static void main(String[] args) {
+        int [] ar={1,2,3,4};
+        System.out.println(Arrays.toString(ar));
+    }
+}
