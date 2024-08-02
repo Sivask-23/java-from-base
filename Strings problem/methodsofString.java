b40485997ab7971a4f2e7757d9243e6ee1fedfc8
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-08-02 20:20:36 +0530

    Add files via upload

---
+++ Strings problem/methodsofString.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class methodsofString {
+    public static void main(String[] args) {
+        String str=" Siva kumar S   ";
+        System.out.println(str);
+        System.out.println(str.trim());
+        System.out.println(str.strip());
+        System.out.println(Arrays.toString(str.split(" ")));
+    }
+}
