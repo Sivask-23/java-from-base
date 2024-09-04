ccff08df8fc1799c3955895c39d2b0e374c93e23
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-09-04 13:18:44 +0530

    Add files via upload

---
+++ Bitwise and Number System/CalcPowerOfANUm.java
@@ -0,0 +1,16 @@
+public class CalcPowerOfANUm {
+    public static void main(String[] args) {
+        int base = 3;
+        int power = 6;
+        int ans=1;
+        while (power > 0) {
+            int lastDigit = power & 1;
+            if (lastDigit==1){
+                ans*=base;
+            }
+            base=base*base;
+            power=power>>1;
+        }
+        System.out.println(ans);
+    }
+}
