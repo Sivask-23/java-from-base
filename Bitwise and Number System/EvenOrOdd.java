dd042e67c0ca2b70f4f494b33ce45a2e7c1e53cd
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-09-06 12:58:27 +0530

    Add files via upload

---
+++ Bitwise and Number System/EvenOrOdd.java
@@ -0,0 +1,13 @@
+public class EvenOrOdd {
+    public static void main(String[] args) {
+        int num=12;
+        int val=num & 1;
+        if (val==1){
+            System.out.println("odd");
+        }
+        else {
+            System.out.println("Even");
+        }
+    }
+
+}
