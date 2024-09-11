145992194a8ed01a6cefcd5f8cece9bddb6201e3
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-09-11 21:30:42 +0530

    Add files via upload

---
+++ Bitwise and Number System/OOPS adv/hello.java
@@ -0,0 +1,8 @@
+import static pack.Message.printMessage;
+
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        printMessage("Siva");
+    }
+}
