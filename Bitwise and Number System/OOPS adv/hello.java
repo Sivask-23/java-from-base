b8beb45f586e71978c7ebad41cd23642383220bf
Sivakumar S <125854895+Sivask-23@users.noreply.github.com>
2024-09-11 21:33:02 +0530

    Delete Bitwise and Number System/OOPS adv directory

--- Bitwise and Number System/OOPS adv/hello.java
+++
@@ -1,8 +0,0 @@
-import static pack.Message.printMessage;
-
-public class hello {
-    public static void main(String[] args) {
-        System.out.println("Hello world");
-        printMessage("Siva");
-    }
-}
